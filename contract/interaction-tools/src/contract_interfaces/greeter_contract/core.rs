eb71875dfa6df59cec03f03177f95f59741b1cd1
Jason <jason@envio.dev>
2024-05-27 17:40:41 +0200

    Try gett code to work by using `add_custom_asset` and `with_tx_policies` - still not working

--- contract/interaction-tools/src/contract_interfaces/greeter_contract/core.rs
+++ contract/interaction-tools/src/contract_interfaces/greeter_contract/core.rs
@@ -1,5 +1,9 @@
+use std::str::FromStr;
+
 use crate::Greeter;
 use crate::Greeting;
+use fuels::types::transaction::TxPolicies;
+use fuels::types::AssetId;
 use fuels::{
     accounts::wallet::WalletUnlocked,
     programs::{call_response::FuelCallResponse, call_utils::TxDependencyExtension},
@@ -15,10 +19,20 @@ pub(crate) async fn set_greeting(
     greeting: String,
 ) -> FuelCallResponse<()> {
     let stringFormatted: SizedAsciiString<8> = greeting.try_into().unwrap();
+
+    let base_asset_id: AssetId =
+        AssetId::from_str("0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07")
+            .unwrap();
+
+    println!("Setting greeting to: {}", stringFormatted.to_string());
+
     contract
         .methods()
         .set_greeting(stringFormatted)
+        // .add_custom_asset(base_asset_id, 2000000, None)
+        // .with_tx_policies(TxPolicies::default().with_max_fee(400000))
         .call()
+        // .simulate()
         .await
         .unwrap()
 }
--- contract/interaction-tools/src/main.rs
+++ contract/interaction-tools/src/main.rs
@@ -29,38 +29,39 @@ async fn main() -> Result<()> {
 
     let phrase = env::var("MNEMONIC").expect("MNEMONIC must be set in .env");
 
-    dbg!("getting the provider");
-    let provider = Provider::connect("testnet.fuel.network").await.unwrap();
-    dbg!("have the provider");
+    let provider = Provider::connect("beta-5.fuel.network").await.unwrap();
 
-    let wallet = WalletUnlocked::new_from_mnemonic_phrase(&phrase, Some(provider)).unwrap();
+    let wallet = WalletUnlocked::new_from_mnemonic_phrase(&phrase, Some(provider.clone())).unwrap();
+
+    let base_asset_id =
+        AssetId::from_str("0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07")
+            .unwrap();
+    let balance = provider
+        .get_asset_balance(wallet.address(), base_asset_id)
+        .await?;
+    let chain_id = provider.chain_id();
+
+    dbg!(balance, chain_id);
+    // call_handler.add_custom_asset(BASE_ASSET_ID, balance, None);
 
     // Get the wallet address. Used later with the faucet
-    dbg!(wallet.address().to_string());
+    println!("Wallet address {}", wallet.address().to_string());
     // From a string.
     let greeter_contract_id =
         ContractId::from_str(GREETER_CONTRACT).expect("failed to create ContractId from string");
 
     // let addressStr: String = wallet.address().to_string();
-    let address_bits: Bits256 = Bits256::from_hex_str(wallet.address().hash.to_string().as_str())
-        .expect("unable to convert address");
-    // dbg!(wallet.address().hash.to_string().as_str());
+    //
     let contract_instance = Greeter::new(greeter_contract_id, wallet);
 
-    dbg!("Getting current greeting");
-    let result = current_greeting(&contract_instance).await.value;
-
-    dbg!("current greeting", result);
-    dbg!("update greeting");
-    let set_greeting_result = set_greeting(&contract_instance, "test".to_string())
+    // // This also fails to read the value.
+    // let result = current_greeting(&contract_instance).await.value;
+    //
+    println!("update greeting");
+    let set_greeting_result = set_greeting(&contract_instance, "test    ".to_string())
         .await
         .value;
     dbg!(set_greeting_result);
-    //
-    // // contract_instance.
-    // //     .get_owner()
-    // //     .await
-    // //     .expect("failed to get owner");
-    //
+
     Ok(())
 }
