3cce249da018765c0820649d8d7a515de3af60dc
Jason <jason@envio.dev>
2024-05-28 13:18:10 +0200

    Update script to make random greetings and clear them too

--- contract/interaction-tools/src/contract_interfaces/greeter_contract/core.rs
+++ contract/interaction-tools/src/contract_interfaces/greeter_contract/core.rs
@@ -1,43 +1,28 @@
-use std::str::FromStr;
-
 use crate::Greeter;
 use crate::Greeting;
-use fuels::accounts::provider::Provider;
 use fuels::types::transaction::TxPolicies;
-use fuels::types::AssetId;
 use fuels::{
-    accounts::wallet::WalletUnlocked,
-    programs::{call_response::FuelCallResponse, call_utils::TxDependencyExtension},
-    types::{Identity, SizedAsciiString},
+    accounts::wallet::WalletUnlocked, programs::call_response::FuelCallResponse,
+    types::SizedAsciiString,
 };
 
 // pub(crate) async fn constructor(contract: &Greeter<WalletUnlocked>) -> FuelCallResponse<()> {
 //     contract.methods().constructor().call().await.unwrap()
 // }
-const TEMP_GAS_LIMIT_HACK: u64 = 300000;
+//
+const TEMP_GAS_LIMIT_HACK: u64 = 400000;
 
 pub(crate) async fn set_greeting(
     contract: &Greeter<WalletUnlocked>,
     greeting: String,
-    // provider: Provider,
 ) -> FuelCallResponse<()> {
-    let stringFormatted: SizedAsciiString<8> = greeting.try_into().unwrap();
-    //
-    // let base_asset_id: AssetId =
-    //     AssetId::from_str("0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07")
-    //         .unwrap();
-
-    // println!("Setting greeting to: {}", stringFormatted.to_string());
-    // let gas_limit = provider.consensus_parameters().max_gas_per_tx() * 0.8;
+    let string_formatted: SizedAsciiString<8> = greeting.try_into().unwrap();
 
     contract
         .methods()
-        .set_greeting(stringFormatted)
-        // .add_custom_asset(base_asset_id, 2000000, None)
-        // .with_tx_policies(TxPolicies::default().with_max_fee(400000))
+        .set_greeting(string_formatted)
         .with_tx_policies(TxPolicies::default().with_script_gas_limit(TEMP_GAS_LIMIT_HACK))
         .call()
-        // .simulate()
         .await
         .unwrap()
 }
--- contract/interaction-tools/src/main.rs
+++ contract/interaction-tools/src/main.rs
@@ -1,10 +1,18 @@
 extern crate dotenv;
+extern crate rand;
+
 use dotenv::dotenv;
+use fuels::prelude::*;
 use fuels::types::ContractId;
-use fuels::{prelude::*, types::Bits256};
+use rand::seq::SliceRandom;
+use rand::thread_rng;
+use rand::Rng;
 use std::env;
 use std::str::FromStr;
+use std::time::Duration;
+use tokio::time::sleep;
 
+use crate::contract_interfaces::greeter_contract::core::clear_greeting;
 use crate::contract_interfaces::greeter_contract::core::{current_greeting, set_greeting};
 
 mod contract_interfaces;
@@ -22,6 +30,16 @@ abigen!(Contract(
 ),);
 const GREETER_CONTRACT: &str = "0x064c6e487c5eec390af0e1d68ff4ee4ef66f54bb749fc6928d46052e027b71f0";
 
+const GREETINGS: [&str; 51] = [
+    "Hello   ", "Hi      ", "Hey     ", "Greeting", "Salute  ", "Howdy   ", "Whats up", "Hola    ",
+    "Bonjour ", "Ciao    ", "Salam   ", "Hallo   ", "Hei     ", "Aloha   ", "Namaste ", "Shalom  ",
+    "Ahoy    ", "Yo      ", "Sup     ", "G'day   ", "Cheers  ", "Good day", "TopOfDaM", "GM      ",
+    "GM GM   ", "GM GM GM", "Welcome ", "Peace   ", "HeyThere", "Hiya    ", "GM earth", "HowdyBro",
+    "Hi World", "Hey you ", "Yo yo yo", "Hi folks", "HeyPeeps", "G-Morgan", "Howdy G ", "Salut   ",
+    "HiFriend", "Hey Bud ", "GM buddy", "GudNight", "HeyBuddy", "Hows it ", "How you?", "WhatsNew",
+    "LookHere", "LongTime", "Niiiiice",
+];
+
 #[tokio::main]
 async fn main() -> Result<()> {
     // Load .env file
@@ -31,48 +49,56 @@ async fn main() -> Result<()> {
 
     let provider = Provider::connect("testnet.fuel.network").await.unwrap();
 
-    /* // This is the implementation, how can I make this code generate 10 wallets for me to use
-        * randomly?
-        * I want to then write 10 greetings to the contract randomly.
-        pub fn new_from_mnemonic_phrase(phrase: &str, provider: Option<Provider>) -> Result<Self> {
-            let path = format!("{DEFAULT_DERIVATION_PATH_PREFIX}/0'/0/0");
-            Self::new_from_mnemonic_phrase_with_path(phrase, provider, &path)
-        }
-    */
-    let wallet = WalletUnlocked::new_from_mnemonic_phrase(&phrase, Some(provider.clone())).unwrap();
+    let wallets = (0..10)
+        .map(|i| {
+            let path = format!("m/44'/1179993420'/0'/0/{}", i);
+            WalletUnlocked::new_from_mnemonic_phrase_with_path(
+                &phrase,
+                Some(provider.clone()),
+                &path,
+            )
+            .unwrap()
+        })
+        .collect::<Vec<_>>();
 
     let base_asset_id =
         AssetId::from_str("0xf8f8b6283d7fa5b672b530cbb84fcccb4ff8dc40f8176ef4544ddb1f1952ad07")
             .unwrap();
-    let balance = provider
-        .get_asset_balance(wallet.address(), base_asset_id)
-        .await?;
-    let chain_id = provider.chain_id();
 
-    dbg!(balance, chain_id);
-    // call_handler.add_custom_asset(BASE_ASSET_ID, balance, None);
+    for wallet in &wallets {
+        let balance = provider
+            .get_asset_balance(wallet.address(), base_asset_id)
+            .await?;
+        // let chain_id = provider.chain_id();
 
-    // Get the wallet address. Used later with the faucet
-    println!("Wallet address {}", wallet.address().to_string());
-    // From a string.
-    let greeter_contract_id =
-        ContractId::from_str(GREETER_CONTRACT).expect("failed to create ContractId from string");
+        if balance < 500000 {
+            println!("Wallet address {}, with balance {}, can use the faucet here: https://faucet-testnet.fuel.network/?address={}", wallet.address(), balance, wallet.address());
+            continue;
+        }
 
-    // let addressStr: String = wallet.address().to_string();
-    //
-    let contract_instance = Greeter::new(greeter_contract_id, wallet);
+        let greeter_contract_id = ContractId::from_str(GREETER_CONTRACT)
+            .expect("failed to create ContractId from string");
 
-    let result = current_greeting(&contract_instance).await.value;
-    dbg!(result);
+        let contract_instance = Greeter::new(greeter_contract_id, wallet.clone());
 
-    println!("update greeting");
-    let set_greeting_result = set_greeting(&contract_instance, "test3   ".to_string())
-        .await
-        .value;
+        let result = current_greeting(&contract_instance).await.value;
+        dbg!(result);
 
-    dbg!(set_greeting_result);
-    let clear_greeting_result = current_greeting(&contract_instance).await.value;
-    dbg!(clear_greeting_result);
+        let mut rng = thread_rng();
+        if rng.gen_ratio(1, 6) {
+            // EVery 1 in 6 times we clear the greeting
+            println!("clear greeting");
+            let clear_greeting_result = clear_greeting(&contract_instance).await;
+            dbg!(clear_greeting_result);
+        } else {
+            let greeting = GREETINGS.choose(&mut rng).unwrap();
+            println!("set greeting to {}", greeting);
+            let set_greeting_result = set_greeting(&contract_instance, greeting.to_string()).await;
+            dbg!(set_greeting_result);
+        }
+
+        sleep(Duration::from_secs(2)).await;
+    }
 
     Ok(())
 }
